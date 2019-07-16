2e511131aa58e05c4ba22c75906d9ea95db16b4e
Danny Kim <dannykim153@gmail.com>
2019-07-16 22:54:24 +0900

    idea

--- preprocessing.py
+++ preprocessing.py
@@ -5,17 +5,27 @@ import pandas as pd
 # PassengerId, Survived, Pclass, Name, Sex, Age, SibSp, Parch, Ticket, Fare, Cabin, Embarked
 
 def load_train_data():
+    # DataFrame
     return pd.read_csv('data/train.csv')
 
 def load_test_data():
+    # DataFrame
     return pd.read_csv('data/test.csv')
 
 
+def fill_na_with_median():
+    pass
+
+def delete_na_row():
+    pass
 
 
+
+# Temporary main method for debugging
+# Should return pre-processed DataFrame
 def main():
     train = load_train_data()
-    print(train)
+    print(train.head())
 
 if __name__ == "__main__":
     main()
