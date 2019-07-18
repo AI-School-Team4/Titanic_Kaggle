3bdf313cbafdf155a69a44f4a70ca127951812a6
sunnydaisy <47199328+sunnydaisy@users.noreply.github.com>
2019-07-18 12:36:42 +0900

    Update preprocessing.py
    add function mapping & median

--- preprocessing.py
+++ preprocessing.py
@@ -5,8 +5,41 @@ from collections import Counter
 # headers
 # PassengerId, Survived, Pclass, Name, Sex, Age, SibSp, Parch, Ticket, Fare, Cabin, Embarked
 
-def fill_na_with_median():
-    pass
+def fill_na_with_median(train, test):
+    train['Age'].fillna(train.groupby('Title')['Age'].transform('median'), inplace=True)
+    test['Age'].fillna(test.groupby('Title')['Age'].transform('median'), inplace=True)
+    
+    return train,test
+
+def mapping(dataset, train, test):
+    title_mapping = {'Mr':0,'Miss':1,'Mrs':2,'Master':3,'Rev':3,'Dr':3,'Col':3,'Major':3,'Mlle':3,'Countess':3,'Ms':3,'Lady':3,'Jonkheer':3,'Don':3,'Dona':3,'Mme':3,'Capt':3,'Sir':3}
+    sex_mapping = {'male':0,'female':1}
+    embarked_mapping = {'S':0,'C':1,'Q':2}
+    cabin_mapping = {'A':0,'B':0.4,'C':0.8,'D':1.2,'E':1.6,'G':2.4,'T':2.8}
+    family_mapping = {1:0,2:0.4,3:0.8,4:1.2,5:1.6,6:2,7:2.4,8:2.8,9:3.2,10:3.6,11:4}
+    
+    for data in dataset:
+        data['Title'] = data['Name'].str.extract('([A-Za-z]+)\.', expand=False)
+        data['Embarked'] = data['Embarked'].fillna('S')
+        data['Cabin'] = data['Cabin'].str[:1]
+        
+    train['FamilySize'] = train['SibSp'] + train['Parch']+1
+    test['FamilySize'] = test['SibSp'] + test['Parch']+1
+    
+    for data in dataset:
+        data['Title'] = data['Title'].map(title_mapping)
+        data['Sex'] = data['Sex'].map(sex_mapping)
+        data['Embarked'] = data['Embarked'].map(embarked_mapping)
+        data['Cabin'] = data['Cabin'].map(cabin_mapping)
+        data['FamilySize'] = data['FamilySize'].map(family_mapping)
+    
+    train.drop('Name',axis=1,inplace=True)
+    test.drop('Name',axis=1,inplace=True)
+    feature_drop = ['Ticket', 'SibSp', 'Parch']
+    train = train.drop(feature_drop, axis=1)
+    test = test.drop(feature_drop, axis=1)
+    
+    return dataset, train, test
 
 def data_target_split(unseparated):
     unseparated["Survived"] = unseparated["Survived"].astype(int)
